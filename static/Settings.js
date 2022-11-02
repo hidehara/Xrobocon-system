42f2b62d2a450b0ad4b680416fa8252618cd9b23
hidehara <hara.hidekazu@gmail.com>
2022-11-02 15:07:33 +0900

    ADD : vscode launch.json MOD : コメントの追加

--- static/Settings.js
+++ static/Settings.js
@@ -6,7 +6,7 @@ class Settings {
 		return category == "T" ? "エントリー部門" : "歩行部門";
 	}
 	static getNumberOfMatch(category) {
-		return category == "T" ? 8 : 8; // 2021 両方8
+		return category == "T" ? 4 : 4; // 各部門の決勝進出数
 	}
 }
 
